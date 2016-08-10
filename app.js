895e402c1180e24565988036e879cb192a783317
violinvirtuoso999 <violinvirtuoso999@yahoo.com>
2016-08-10 16:41:46 -0500

    Delete app.js

--- app.js
+++
@@ -1,6 +0,0 @@
-var electron = require('electron')  
-       
-electron.app.on('ready', function () {  
-  var mainWindow = new electron.BrowserWindow({width: 600, height: 700});  
-  mainWindow.loadURL('file://' + __dirname + '/index.html');
-});
