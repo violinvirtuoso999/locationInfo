25bda5321217ef894a84af07b732cb9d23cc565a
CARROLL <violinvirtuoso999@yahoo.com>
2016-08-10 16:47:41 -0500

    Added sample-config.js

---
+++ config/sample-config.js
@@ -0,0 +1,4 @@
+const mapAccessToken = 'YOUR-MAPBOX-ACCESS-TOKEN';
+const geoCodeApiKey = 'YOUR-GOOGLE-MAPS-API-KEY';
+
+module.exports = {mapAccessToken:mapAccessToken, geoCodeApiKey:geoCodeApiKey};
